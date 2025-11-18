6590ca4217299fe82bd45af6d93f6abe843be740
Lennart van der Molen <lennartvandermolen@gmail.com>
2025-11-18 18:49:06 +0100

    Restore app bootstrap and rendering

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import PreciseCollisionGame from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error('QubeGame root container not found');
+}
+
+const root = createRoot(container);
+
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line no-console
+  console.info('QubeGame bootstrap running', process.env.NODE_ENV);
+}
+
+root.render(
   <React.StrictMode>
     <PreciseCollisionGame />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
